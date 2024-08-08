2719c1afdbff49a0197a7c9d39f479d864e0f74c
Nicolas <nicovommaro02@gmail.com>
2024-08-08 12:56:13 -0300

    r

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -12,6 +12,7 @@
           <!-- Slide 2: Image with Text -->
           <div class="carousel-item w-full h-72 md:h-96 relative">
             <div class="absolute inset-0 bg-cover bg-center opacity-50" :style="{ backgroundImage: `url('/public/banner2.png')` }"></div>
+            <img src="/public/logo_tipografia_mobile.png" alt="San Semillero Banner" class="w-full h-full object-none">
             <div class="relative z-10 flex items-center justify-center h-full p-6 bg-black bg-opacity-50">
               <div class="text-center text-white">
                 <h1 class="text-lg md:text-4xl font-bold mb-4">San Semillero</h1>
